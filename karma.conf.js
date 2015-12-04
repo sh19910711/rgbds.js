1c4f9f36f53f2e9a00b27ee2b9b48a19541ba28e
Hiroyuki Sano <sh19910711@gmail.com>
2015-12-04 20:41:39 +0900

    Use karma-html2js-preprocessor

--- karma.conf.js
+++ karma.conf.js
@@ -19,7 +19,8 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'dist/rgbds.js',
-      'spec/*_spec.js'
+      'spec/*_spec.js',
+      'spec/rgbasm/*',
     ],
 
 
@@ -31,7 +32,8 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      '*.js': ['babel']
+      '*.js': ['babel'],
+      '**/*.asm': ['html2js']
     },
 
     babelPreprocessor: {
