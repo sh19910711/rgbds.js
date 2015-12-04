f539a6a89505a5f0891275fc997ffaf5e9186a75
Hiroyuki Sano <sh19910711@gmail.com>
2015-12-04 20:45:14 +0900

    Replace tabs

--- spec/rgbasm_spec.js
+++ spec/rgbasm_spec.js
@@ -1,7 +1,7 @@
 describe('rgbasm()', () => {
-	it('should be a function', () => {
-		expect(rgbasm).toBeFunction()
-	})
+  it('should be a function', () => {
+    expect(rgbasm).toBeFunction()
+  })
 
   it('halt', (done) => {
     console.log(__html__['spec/rgbasm/hello.asm'])
