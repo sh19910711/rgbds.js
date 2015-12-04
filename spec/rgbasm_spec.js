ef9fa869235ef18ebd3163858fddcb5da19c798e
Hiroyuki Sano <sh19910711@gmail.com>
2015-12-04 20:55:51 +0900

    Check rgbasm() output

--- spec/rgbasm_spec.js
+++ spec/rgbasm_spec.js
@@ -4,12 +4,12 @@ describe('rgbasm()', () => {
   })
 
   it('halt', (done) => {
-    console.log(__html__['spec/rgbasm/hello.asm'])
     window.Module = {
       input: __html__['spec/rgbasm/hello.asm']
     }
-    rgbasm(Module['input'], _=> {
-      console.log('done?')
+    rgbasm(Module['input'], ret => {
+      str = String.fromCharCode.apply(null, ret.slice(0, 4))
+      expect(str).toBe('RGB2')
       done()
     })
   })
