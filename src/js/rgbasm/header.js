7a5b19eca1667489fc3b9a3d7757992e5d6b3d1f
Hiroyuki Sano <sh19910711@gmail.com>
2015-12-04 22:24:15 +0900

    Add rgbasm()

--- src/js/rgbasm/header.js
+++ src/js/rgbasm/header.js
@@ -1,18 +1,15 @@
 function rgbasm(options, callback) {
   var Module = {}
+
+  Object.assign(Module, options)
+
   Object.assign(Module, {
     canvas: document.createElement('canvas'),
     callback: callback,
-    arguments: ['-ooutput.obj', 'input.asm'],
+    arguments: ['-ooutput.obj', 'halt.asm'],
 
     preRun: _=> {
-      FS.createDataFile(
-        '/',
-        'input.asm',
-        Module['input'],
-        true,
-        false
-      )
+      Object.keys(Module.files).forEach( filename => createProjectFile(filename) )
     },
 
     postRun: _=> {
@@ -23,7 +20,16 @@ function rgbasm(options, callback) {
       FS.read(stream, buf, 0, length, 0)
       FS.close(stream)
       callback(buf)
-    }
-  });
+    },
+  })
 
-  Object.assign(Module, options)
+  function createProjectFile(filename) {
+    FS.createDataFile(
+      '/',
+      filename,
+      Module.files[filename],
+      true,
+      false
+    )
+  }
+
--- src/js/rgblink/header.js
+++ src/js/rgblink/header.js
@@ -1,7 +1,30 @@
-function rgblink(opts, callback) {
-  var Module = {
-    input: opts.input,
+function rgblink(options, callback) {
+  var Module = {}
+
+  Object.assign(Module, {
     canvas: document.createElement('canvas'),
     callback: callback,
-    arguments: ['-ooutput.obj', 'input.asm'],
-  };
+    arguments: ['-ooutput.gb', 'input.obj'],
+
+    preRun: _=> {
+      FS.createDataFile(
+        '/',
+        'input.obj',
+        Module['input'],
+        true,
+        false
+      )
+    },
+
+    postRun: _=> {
+      const output = '/output.obj'
+      const length = FS.stat(output).size
+      const stream = FS.open(output, 'r')
+      const buf = new Uint8Array(length)
+      FS.read(stream, buf, 0, length, 0)
+      FS.close(stream)
+      callback(buf)
+    }
+  });
+
+  Object.assign(Module, options)
