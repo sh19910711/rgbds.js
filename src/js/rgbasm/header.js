d79b4e64c968703a89ca471bb373ca1cc6e62561
Hiroyuki Sano <sh19910711@gmail.com>
2015-12-04 20:41:09 +0900

    Remove pre.js

--- src/js/rgbasm/header.js
+++ src/js/rgbasm/header.js
@@ -4,4 +4,24 @@ function rgbasm(opts, callback) {
     canvas: document.createElement('canvas'),
     callback: callback,
     arguments: ['-ooutput.obj', 'input.asm'],
+
+		preRun: _=> {
+			FS.createDataFile(
+				'/',
+				'input.asm',
+				Module['input'],
+				true,
+				false
+			)
+		},
+
+		postRun: _=> {
+			const output = '/output.obj'
+			const length = FS.stat(output).size
+			const stream = FS.open(output, 'r')
+			const buf = new Uint8Array(length)
+			FS.read(stream, buf, 0, length, 0)
+			FS.close(stream)
+			callback(buf)
+		}
   };
